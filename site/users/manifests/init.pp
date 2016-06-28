37ee9262fb2682f3a985e44261df6f6fb6dc700c
JohnAtCL <johnnguyen@corelogic.com>
2016-06-28 13:56:12 -0700

    Update init.pp

--- site/users/manifests/init.pp
+++ site/users/manifests/init.pp
@@ -1,3 +1,7 @@
 class users {
-  notify {'Add new users class': }
+  user { 'fundamentals':
+    ensure => present,
+    # password => 'puppet8#labs', # Windows requires a plain text password
+    # groups => ['Users'], # Display in Windows Control Panel
+  }
 }
