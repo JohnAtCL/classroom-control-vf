29f9fcbddad4f4cf515750a4d5ca2739c7f99264
JohnAtCL <johnnguyen@corelogic.com>
2016-06-30 08:31:49 -0700

    revert

--- site/users/manifests/init.pp
+++ site/users/manifests/init.pp
@@ -1,9 +1,7 @@
-#class users {
-#  user { 'fundamentals':
-#    ensure => present,
-#    # password => 'puppet8#labs', # Windows requires a plain text password
-#    # groups => ['Users'], # Display in Windows Control Panel
-#  }
-#}
-
-include users::admins
+class users {
+  user { 'fundamentals':
+    ensure => present,
+    # password => 'puppet8#labs', # Windows requires a plain text password
+    # groups => ['Users'], # Display in Windows Control Panel
+  }
+}
